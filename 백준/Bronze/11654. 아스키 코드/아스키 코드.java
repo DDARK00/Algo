884b9116265e7b68bd8623f2e497088ff9028f7d
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-01-11 16:19:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        //Scanner sc = new Scanner(System.in);
+        int n = System.in.read();
+        
+        System.out.println(n);
+
+    }
+}
