3a8a1dd012ccfba6059615966ca13661520781e8
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-01-12 17:24:26 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 224 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int k, q, r, b, n, p;
+        k =1-sc.nextInt();
+        q =1-sc.nextInt();
+        r =2-sc.nextInt();
+        b =2-sc.nextInt();
+        n =2-sc.nextInt();
+        p =8-sc.nextInt();
+
+        System.out.print(k+" ");
+        System.out.print(q+" ");
+        System.out.print(r+" ");
+        System.out.print(b+" ");
+        System.out.print(n+" ");
+        System.out.print(p);
+    }
+}
