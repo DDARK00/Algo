8fd40599281c6125d219b02c1923d2412669af83
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-01-12 17:15:59 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String args[]){
+        String arr[]=
+        {
+            "         ,r'\"7",
+            "r`-_   ,'  ,/",
+            " \\. \". L_r'",
+            "   `~\\/",
+            "      |",
+            "      |"
+        };
+        for(String i : arr){
+            System.out.println(i);
+        }
+    }
+}
