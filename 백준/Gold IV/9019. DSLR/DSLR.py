e293c8666513e97c857ae4f80949c3ab55c96eb5
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-02-16 10:32:53 +0900

    [Gold IV] Title: DSLR, Time: 8984 ms, Memory: 218208 KB -BaekjoonHub

--- 백준/Gold IV/9019. DSLR/DSLR.py
+++ 백준/Gold IV/9019. DSLR/DSLR.py
@@ -8,7 +8,7 @@ T = int(input())
 
 def delta(num, command):
     if command == 0:
-        return num * 2 % 10000, 'D'
+        return (num << 1) % 10000, 'D'
     if command == 1:
         return num - 1 if num - 1 > -1 else 9999, 'S'
     if command == 2:
@@ -18,23 +18,22 @@ def delta(num, command):
 
 
 for _ in range(T):
-    start, end = input().split()
+    start, end = map(int, input().split())
 
     # D 더블 2n mod 10000
     # S -1
     # L 왼쪽회전 1234 - 2341
     # R 오른쪽회전 1234 -4123
 
-    q = deque([(int(start), '')])
+    q = deque([(start, '')])
     # bfs
     visited = [0] * 10000
 
     while q:
         v, o = q.popleft()
         # 변환된 숫자, 변환 타입
-        if v == int(end):
+        if v == end:
             print(o)
-            # 와 이런 방법이 있었구나 하.........걍 부모 노드 정보를 자식 노드에 넘겨주고 계속 이어가면 되는 것이었따....
             # 내 고민은 무엇인가...
             break
 
@@ -46,5 +45,4 @@ for _ in range(T):
                 # 4중변환
 
                 if not visited[d]:
-                    q.append((d, o+c))
-                    # 부모 노드 기억...? 어떻게 깔끔하게 하지
+                    q.append((d, o + c))
