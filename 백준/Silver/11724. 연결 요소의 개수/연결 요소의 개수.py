0e83b3d00b92ecd5e04d68f50e48b232c8d3a709
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-02-20 09:08:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 856 ms, Memory: 71160 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+g = {}
+
+for i in range(1, n + 1):
+    g[i] = []
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    g[a].append(b)
+    g[b].append(a)
+
+# print(g)
+
+q = deque([])
+# bfs
+
+visited = {}
+
+cnt = 0
+
+for val in g.keys():
+    if not visited.get(val):
+        q.append(val)
+
+        cnt += 1
+
+        while q:
+            v = q.popleft()
+            if not visited.get(v):
+                visited[v] = 1
+                if g.get(v):
+                    q.extend(g[v])
+
+print(cnt)
+# print(g)
