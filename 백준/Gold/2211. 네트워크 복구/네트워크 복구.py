1b90165c08ee63fd10f68538486780027ceeda79
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-04-17 14:15:53 +0900

    [Gold II] Title: 네트워크 복구, Time: 472 ms, Memory: 78100 KB -BaekjoonHub

---
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.py
@@ -0,0 +1,42 @@
+import sys
+from math import inf
+from heapq import heappop, heappush
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+# n개의 컴퓨터, m개의 회선
+# 1번 컴퓨터는 보안 시스템을 설치할 슈퍼컴퓨터이다.
+pq = [(0, 1)]  # w, v
+
+g = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # 양방향
+    g[a].append((c, b))  # w, v
+    g[b].append((c, a))  # w, v
+
+# print(g)
+# dijk
+cost = [[inf, 0] for _ in range(1 + n)]
+cost[1][0] = 0
+while pq:
+    w, v = heappop(pq)
+
+    if cost[v][0] < w:
+        continue
+
+    for nw, nv in g[v]:
+        if cost[nv][0] > nw + w:
+            cost[nv][0] = nw + w
+            cost[nv][1] = v
+            heappush(pq, (nw + w, nv))
+
+way = set()
+for i in range(2, n + 1):
+    start = cost[i][1]
+    end = i
+    way.add((end, start))
+print(len(way))
+for s, e in list(way):
+    print(s, e)
