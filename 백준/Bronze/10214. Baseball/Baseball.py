b75efdabbab1896c8f2dba938cbe63202aca11b8
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-08-05 08:51:09 +0900

    [Bronze III] Title: Baseball, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    y = 0
+    k = 0
+    for _ in range(9):
+        a, b =map(int,input().split())
+        y+=a
+        k+=b
+    if y>k:
+        print("Yonsei")
+    elif y<k:
+        print("Korea")
+    else:
+        print("Draw")
