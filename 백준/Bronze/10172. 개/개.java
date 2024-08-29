263cb8fb81a9a9c39a846bcd476eb1d532d9aa87
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-08-29 14:00:04 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args){
+        
+        String[] arr= {
+            "|\\_/|",
+            "|q p|   /}",
+            "( 0 )\"\"\"\\",
+            "|\"^\"`    |",
+            "||_/=\\\\__|"  
+        };
+
+    for(String arg:arr){
+        System.out.println(arg);
+    }
+}
+
+}
