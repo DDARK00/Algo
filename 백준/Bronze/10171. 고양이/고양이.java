be58cb87a41f674f9e6591d5a0020928c2cc9e0a
DDARK <156265425+DDARK00@users.noreply.github.com>
2024-08-29 14:00:20 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        
+        String[] arr= {
+            "\\    /\\",
+            " )  ( ')",
+            "(  /  )",
+            " \\(__)|"    
+        };
+
+    for(String arg:arr){
+        System.out.println(arg);
+    }
+}
+
+}
