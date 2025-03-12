96281016860fc2cd30d44093b589dfaf755f676c
DDARK <156265425+DDARK00@users.noreply.github.com>
2025-03-12 14:32:55 +0900

    [Bronze IV] Title: 왜 맘대로 예약하냐고, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32941. 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.py
@@ -0,0 +1,9 @@
+t, x = map(int, input().split())
+for _ in range(int(input())):
+    input()
+    if x in map(int,input().split()):
+        continue
+    else:
+        print("NO")
+        exit()
+print("YES")
