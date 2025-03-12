0159c090fd55f7dd6382204840b2a729f9f4af8e
DDARK <156265425+DDARK00@users.noreply.github.com>
2025-03-12 14:52:50 +0900

    [Gold V] Title: 래환이의 택시 타기 대작전, Time: 156 ms, Memory: 114360 KB -BaekjoonHub

--- 백준/Gold/32530. 래환이의 택시 타기 대작전/래환이의 택시 타기 대작전.py
+++ 백준/Gold/32530. 래환이의 택시 타기 대작전/래환이의 택시 타기 대작전.py
@@ -1,23 +1,22 @@
 import sys
 input=sys.stdin.readline
-
 ans = 0
 st = []
 n = int(input())
-lst = []
-for _ in range(n):
-    hh, mm = map(int, input().rstrip().split(":"))
-    hhmm = hh*60+mm
-    if hhmm>=1440:
-        hhmm = 1439
-    lst.append(hhmm)
+lst = [0]*n
+for i in range(n):
+    hh,mm = map(int, input().rstrip().split(":"))
+    hm = hh*60+mm
+    if hm>=1440:
+        hm = 1439
+    lst[i] = hm
 
 lst.sort()
 
 for num in lst:
     if not st:
         ans+=1
-        st.append(num)
+        st = [num]
     else:
         s = st[0] + 10
         e = num - 10
