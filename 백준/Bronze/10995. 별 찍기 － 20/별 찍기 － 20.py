cc257d926dc7147db17bd9038cfcfdcd9e061c1a
DDARK <156265425+DDARK00@users.noreply.github.com>
2026-04-15 17:35:11 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
@@ -1,6 +1,1 @@
-import sys
-input=sys.stdin.readline
-n=int(input())
-for i in range(n):
-    i%2 and print(" *"*n)
-    i%2==0 and print("* "*n)
+for i in range(n:=int(input())):print((" *"*n)[~i%2:])
