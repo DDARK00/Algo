5251e044b554853e68c77c78086aa8e625d9df0e
DDARK <156265425+DDARK00@users.noreply.github.com>
2026-04-15 18:45:39 +0900

    [Silver I] Title: 마니또, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/5107. 마니또/마니또.cc
+++ 백준/Silver/5107. 마니또/마니또.cc
@@ -1,12 +1,42 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, tc=1;
+    cin >> n;
+    while (n!=0) {
+        string a, b;
+        map<string,string> data;
+        map<string,bool> visited;
+        vector<string> saram;
+        for (int i=0; i<n; i++) {
+            cin >> a >> b;
+            data[a]=b;
+            saram.push_back(a);
+        }
+
+        int answer=0;
+        string daum_saram="";
+        for (auto s : saram) {
+            if (!visited[s]) {
+                answer++;
+                daum_saram=data[s];
+                visited[s]=1;
+                while (!visited[daum_saram]) {
+                    visited[daum_saram]=1;
+                    daum_saram=data[daum_saram];
+                }
+            }
+        }
+
+        cout << tc << " " << answer << "\n";
+        cin >> n;
+        tc++;
+    }
+    return 0;
+}
