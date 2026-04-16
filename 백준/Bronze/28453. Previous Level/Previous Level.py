2a4378cef96f357c9905139de3c8dbad01ab50a2
DDARK <156265425+DDARK00@users.noreply.github.com>
2026-04-16 15:50:43 +0900

    [Bronze IV] Title: Previous Level, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28453. Previous Level/Previous Level.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for num in map(int, input().split()):
+    if num==300:
+        print(1, end=' ')
+    elif num>=275:
+        print(2, end=' ')
+    elif num>=250:
+        print(3, end=' ')
+    else:
+        print(4, end=' ')
